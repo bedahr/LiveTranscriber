2dfbe4d4b231c03f5ebd1efa233d8831318b3fda
Simon <simon@polyglotfoundation.org>
2013-08-02 15:44:42 +0200

    fixed segment importer

--- code/media/recordings/lib/recording/segment_importer.rb
+++ code/media/recordings/lib/recording/segment_importer.rb
@@ -7,7 +7,7 @@ class Recording
       raise "recording already has segments" if recording.segments.exists?
 
       File.readlines(filename).collect(&:rstrip).each do |line|
-        start_time, end_time, body = tabs.shift(4)
+        start_time, end_time, body = line.split(/\t/, 3)
 
         attributes = { start_time: start_time, end_time: end_time,
                        body: body
