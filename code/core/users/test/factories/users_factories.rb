589ed62affe328f529b0b0fda639f0170fd12edd
Simon <simon@polyglotfoundation.org>
2013-08-04 13:34:54 +0200

    added user factory

---
+++ code/core/users/test/factories/users_factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:email)      { |seq| "user#{seq}@foo.com" }
+    sequence(:name)       { |seq| "User#{seq}" }
+
+    password              { name + "password" }
+    password_confirmation { name + "password" }
+
+    initialize_with { User.where(:name => name).first || User.new(attributes) }
+  end
+end
