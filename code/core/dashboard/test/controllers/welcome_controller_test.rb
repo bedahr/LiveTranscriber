e21405635f8f2e4f2c87d7e8c96d1ce1b89157f9
Simon <simon@polyglotfoundation.org>
2013-08-04 19:02:38 +0200

    fixed css for dashboard

--- code/core/dashboard/test/controllers/welcome_controller_test.rb
+++ code/core/dashboard/test/controllers/welcome_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  before do
+    login_as("master_test")
+  end
+
+  test "should get homepage" do
+    get :index
+    assert_response :success
+  end
+
 end
