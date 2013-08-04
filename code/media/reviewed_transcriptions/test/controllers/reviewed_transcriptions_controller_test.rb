f425b1c50d0dd78230b71b4105e8d4fcd62e1cb7
Simon <simon@polyglotfoundation.org>
2013-08-04 20:13:51 +0200

    removed obsolete actions

--- code/media/reviewed_transcriptions/test/controllers/reviewed_transcriptions_controller_test.rb
+++ code/media/reviewed_transcriptions/test/controllers/reviewed_transcriptions_controller_test.rb
@@ -13,39 +13,4 @@ class ReviewedTranscriptionsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:reviewed_transcriptions)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  # test "should create" do
-  #   assert_difference('ReviewedTranscription.count') do
-  #     post :create, reviewed_transcription:  factory_attributes_with_associations_for(:reviewed_transcription)
-  #   end
-  #
-  #   assert_redirected_to reviewed_transcription_path(assigns(:reviewed_transcription))
-  # end
-
-  test "should show" do
-    get :show, id: @reviewed_transcription
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @reviewed_transcription
-    assert_response :success
-  end
-
-  # test "should update" do
-  #   put :update, id: @reviewed_transcription, reviewed_transcription: FactoryGirl.attributes_for(:reviewed_transcription)
-  #   assert_redirected_to reviewed_transcription_path(assigns(:reviewed_transcription))
-  # end
-
-  test "should destroy" do
-    assert_difference('ReviewedTranscription.count', -1) do
-      delete :destroy, id: @reviewed_transcription
-    end
-
-    assert_redirected_to reviewed_transcriptions_path
-  end
 end
